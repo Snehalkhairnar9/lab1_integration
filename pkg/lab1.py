93f3e2a1b0ad425a4021db35529c047455438fac
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-10-20 16:06:45 +0100

    Python_code_test

---
+++ pkg/lab1.py
@@ -0,0 +1,15 @@
+# File          : lab1.py
+# Author        : Snehal Khairnar
+# Version       : v1.0
+# Created Date  : 20/10/2021
+# Description   : Testing the python code
+# Licensing     : Snehal Khairnar, LYIT
+# ----------------------------------
+
+a = 50
+b = 60
+c = a + b
+if c == 110:
+    print("Test passed")
+else:
+    print("Test failed")
